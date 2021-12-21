f82db63364cac3f96979dda7f4a85d6e5e656c66
赵金柱 <zhaojinzhu@hnshengdun.com>
2021-12-21 13:01:01 +0800

    修改devserver

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-11-18 09:20:51
  * @LastEditors: Aiva
- * @LastEditTime: 2021-11-18 10:44:11
+ * @LastEditTime: 2021-12-21 12:53:27
  * @FilePath: \yilin-music\craco.config.js
  */
 const CracoLessPlugin = require('craco-less');
@@ -9,6 +9,9 @@ const themeConfig = require('./theme.config')
 const path = require('path')
 const resolve = (dir) => path.resolve(__dirname, dir);
 module.exports = {
+    devServer:{
+        port:5000,
+    },
     webpack: {
         alias: {
             '@': resolve('src'),
